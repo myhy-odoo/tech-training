8f52b614faa4ebb743851ad8c8874bfe75a13149
Mylyna Hy <myhy@odoo.com>
2022-03-08 00:06:58 +0000

    add spaceship model

---
+++ odoo-spaceship/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Odoo Space Mission",
+    "summary": "Mission to go to space",
+    "description": "Let's go to  space with Odoo",
+    "author": "Odoo",
+    "website": "https://www.odoo.com",
+    "category": "Training",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [],
+    "demo": [] 
+}
---
+++ odoo-spaceship/model/.ipynb_checkpoints/spaceship-checkpoint.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Spaceship(model.Models){
+    
+    _name = "spaceMission.spaceship"
+    _description = "Spaceship"
+    
+    active = fields.Boolean(string = "Active", default = True)
+    numOfPassengers = fields.Integer(string = "Number of Passengers")
+    shipType = fields.Selection(string = "Type of Ship",selection = [("shipX","ShipX"),"shipY","ShipY")], copy = False)
+    
+    fuelType = fields.Selection(string = "Type of Fuel", selection = [("gas","Gas"),("diesel","Diesel")], copy = False)
+    
+    dimensions = fields.Char(string = "Dimensions(length x width x height)")
+}
