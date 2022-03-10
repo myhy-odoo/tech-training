6e6de23a27d2becc8fd0e13e1b3673db2557387d
Mylyna Hy <myhy@odoo.com>
2022-03-10 21:49:31 +0000

    inherited sale order

--- odoo-academy/__manifest__.py
+++ odoo-academy/__manifest__.py
@@ -7,7 +7,7 @@
     "website": "https://www.odoo.com",
     "category": "Training",
     "version": "0.1",
-    "depends": ["base"],
-    "data": [ "security/academy_security.xml", "security/ir.model.access.csv","views/academy_menuItems.xml","views/course_views.xml","views/session_views.xml"],
+    "depends": ["sale"],
+    "data": [ "security/academy_security.xml", "security/ir.model.access.csv","views/academy_menuItems.xml","views/course_views.xml","views/session_views.xml","views/sale_views_inherit.xml"],
     "demo": [ "demo/academy_demo.xml"] 
 }
