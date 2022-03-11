16f3ebb8c0e7694209491c631f63e866d2029ecb
Mylyna Hy <myhy@odoo.com>
2022-03-11 17:37:58 +0000

    some fixes to report

--- odoo-academy/__manifest__.py
+++ odoo-academy/__manifest__.py
@@ -7,7 +7,7 @@
     "website": "https://www.odoo.com",
     "category": "Training",
     "version": "0.1",
-    "depends": ["sale"],
+    "depends": ["base","sale"],
     "data": [ "security/academy_security.xml", "security/ir.model.access.csv","views/academy_menuItems.xml","views/course_views.xml","views/session_views.xml","views/sale_views_inherit.xml","views/product_views_inherit.xml", "wizard/sale_wizard_view.xml","report/session_report_templates.xml"],
     "demo": [ "demo/academy_demo.xml"] 
 }
